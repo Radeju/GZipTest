b49980440e54cb240aa8be763773f1943bcab172
radoslaw jurewicz <radejur@gmail.com>
2018-04-30 23:27:44 +0200

    Addef missing file

---
+++ GZipTest/GZipTest/Tools/FileManipulator.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using GZipTest.Globals;
+
+namespace GZipTest.Tools
+{
+    public class FileManipulator
+    {
+        public List<FileInfo> Split(string file, int chunkSizeInMbs, string chunkName, string folderName = null)
+        {
+            List<FileInfo> createdFiles = new List<FileInfo>();
+            int chunkSize = chunkSizeInMbs * 1024 * 1024;
+            byte[] buffer = new byte[Const.BUFFER_SIZE];
+
+            using (FileStream input = File.OpenRead(file))
+            {
+                int index = 0;
+                if (folderName != null)
+                {
+                    Directory.CreateDirectory(folderName);
+                }
+
+                while (input.Position < input.Length)
+                {
+                    string nextFile = chunkName + index;
+                    string nextPath = folderName == null ? nextFile : folderName + "\\" + nextFile;
+                    FileInfo nextFileInfo = new FileInfo(nextFile);
+
+                    using (FileStream output = File.Create(nextPath))
+                    {
+                        createdFiles.Add(nextFileInfo);
+                        int bytesRead;
+                        int remaining = chunkSize;
+                        while (remaining > 0 && 
+                               (bytesRead = input.Read(buffer, 0, Math.Min(remaining, Const.BUFFER_SIZE))) > 0)
+                        {
+                            output.Write(buffer, 0, bytesRead);
+                            remaining -= bytesRead;
+                        }
+                    }
+                    index++;
+                }
+            }
+
+            return createdFiles;
+        }
+
+        public void Merge(List<FileInfo> filesToMerge, string outFileName)
+        {
+            byte[] buffer = new byte[Const.BUFFER_SIZE];
+            using (FileStream outFileStream = File.Create(outFileName))
+            {
+                foreach (var fi in filesToMerge)
+                {
+                    using (FileStream nextFile = File.OpenRead(fi.Name))
+                    {
+                        int bytesRead;
+                        while ( (bytesRead = nextFile.Read(buffer, 0, buffer.Length)) != 0)
+                        {
+                            outFileStream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                    fi.Delete();
+                }
+            }
+        }
+    }
+}
